42933f8b4f93dcb304c23c852cfb63950bf12fa6
Brad Rydzewski <brad.rydzewski@gmail.com>
2015-03-05 00:18:25 -0800

    cleanup file

--- common/config.go
+++ common/config.go
@@ -1,51 +1,29 @@
 package common
 
 type Config struct {
-	/*
-		Image    string
-		Env      []string `json:"env"`
-		Script   []string `json:"script"`
-		Branches []string
-		Services []string
-
-		Git struct {
-			Path string
-		}
-
-			Docker struct {
-				Volumes    []string
-				Privileged bool
-				Hostname   string
-				Net        string
-			}
-	*/
-
-	//Clone   map[string]Step
-	//Build   map[string]Step
-
 	Clone Step
 	Build Step
 
 	Compose map[string]Step
-	Publish map[string]Step //interface{}
-	Deploy  map[string]Step //interface{}
-	Notify  map[string]Step //interface{}
+	Publish map[string]Step
+	Deploy  map[string]Step
+	Notify  map[string]Step
 
 	Matrix map[string][]string
 }
 
 type Step struct {
-	Name        string   `json:"name"`
-	Image       string   `json:"image"`
-	Environment []string `json:"environment"`
-	Volumes     []string `json:"volumes"`
-	Hostname    string   `json:"hostname"`
-	Privileged  bool     `json:"privileged"`
-	Net         string   `json:"net"`
+	Name        string
+	Image       string
+	Environment []string
+	Volumes     []string
+	Hostname    string
+	Privileged  bool
+	Net         string
 
 	// Config represents the unique configuration details
 	// for each plugin.
-	Config map[string]interface{} `yaml:"config,inline" json:"config"`
+	Config map[string]interface{} `yaml:"config,inline"`
 }
 
 /*
