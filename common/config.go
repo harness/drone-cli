4e57f9106c86710ee33c53c0a6d05d81b1c18327
Brad Rydzewski <brad.rydzewski@gmail.com>
2015-03-05 17:53:10 -0800

    broke commit and step into separate files

--- common/config.go
+++ common/config.go
@@ -1,6 +1,8 @@
 package common
 
+// A config structure is used to configure a build.
 type Config struct {
+	Init  Step
 	Clone Step
 	Build Step
 
@@ -11,66 +13,3 @@ type Config struct {
 
 	Matrix map[string][]string
 }
-
-type Step struct {
-	Name        string
-	Image       string
-	Environment []string
-	Volumes     []string
-	Hostname    string
-	Privileged  bool
-	Net         string
-
-	// Config represents the unique configuration details
-	// for each plugin.
-	Config map[string]interface{} `yaml:"config,inline"`
-}
-
-/*
-
-type Container interface {
-	Create()
-	Start()
-	Wait()
-	Logs()
-	Info()
-	Stop()
-	Kill()
-	Remove()
-}
-
-clone:
-  git:
-	  path: foo/bar
-
-build:
-  script:
-	  image: go:1.2
-	  commands:
-		  - go build
-			- go test
-
-type Step struct {
-	Image       string
-	Commands    []string
-	Links       []string
-	Ports       []string
-	Expose      []string
-	Volumes     []string
-	VolumesFrom []string
-	Environment []string
-	DNS         []string
-	Net         string
-
-	CapAdd     []string
-	CapDrop    []string
-	Workdir    string
-	Entrypoint string
-	User       string
-	Hostname   string
-	Privileged bool
-
-	MemLimit  int64
-	CPUShared int
-}
-*/
