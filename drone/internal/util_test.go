06de2e587d63f8af4dafaf6281496182a0acf0c9
Regan Chan <regan.chan@vidyard.com>
2018-01-30 15:17:46 -0500

    Allow values in key/value pairs to contain '=' characters

--- drone/internal/util_test.go
+++ drone/internal/util_test.go
@@ -3,10 +3,13 @@ package internal
 import "testing"
 
 func TestParseKeyPair(t *testing.T) {
-	s := []string{"FOO=bar", "BAR=", "INVALID"}
+	s := []string{"FOO=bar", "BAR=", "BAZ=qux=quux", "INVALID"}
 	p := ParseKeyPair(s)
 	if p["FOO"] != "bar" {
 		t.Errorf("Wanted %q, got %q.", "bar", p["FOO"])
+	}
+	if p["BAZ"] != "qux=quux" {
+		t.Errorf("Wanted %q, got %q.", "qux=quux", p["BAZ"])
 	}
 	if _, exists := p["BAR"]; !exists {
 		t.Error("Missing a key with no value. Keys with empty values are also valid.")
