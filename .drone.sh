c9e971a3580404b37e420afefebf3e0392171dd1
Lauris BH <lauris@nix.lv>
2019-08-07 11:16:29 +0300

    For windows release binary should be named drone.exe

--- .drone.sh
+++ .drone.sh
@@ -6,17 +6,17 @@ set -x
 export CGO_ENABLED=0
 
 # compile for all architectures
-GOOS=linux   GOARCH=amd64 go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/linux/amd64/drone   github.com/drone/drone-cli/drone
-GOOS=linux   GOARCH=arm64 go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/linux/arm64/drone   github.com/drone/drone-cli/drone
-GOOS=linux   GOARCH=arm   go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/linux/arm/drone     github.com/drone/drone-cli/drone
-GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/windows/amd64/drone github.com/drone/drone-cli/drone
-GOOS=darwin  GOARCH=amd64 go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/darwin/amd64/drone  github.com/drone/drone-cli/drone
+GOOS=linux   GOARCH=amd64 go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/linux/amd64/drone       github.com/drone/drone-cli/drone
+GOOS=linux   GOARCH=arm64 go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/linux/arm64/drone       github.com/drone/drone-cli/drone
+GOOS=linux   GOARCH=arm   go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/linux/arm/drone         github.com/drone/drone-cli/drone
+GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/windows/amd64/drone.exe github.com/drone/drone-cli/drone
+GOOS=darwin  GOARCH=amd64 go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/darwin/amd64/drone      github.com/drone/drone-cli/drone
 
 # tar binary files prior to upload
 tar -cvzf release/drone_linux_amd64.tar.gz   -C release/linux/amd64   drone
 tar -cvzf release/drone_linux_arm64.tar.gz   -C release/linux/arm64   drone
 tar -cvzf release/drone_linux_arm.tar.gz     -C release/linux/arm     drone
-tar -cvzf release/drone_windows_amd64.tar.gz -C release/windows/amd64 drone
+tar -cvzf release/drone_windows_amd64.tar.gz -C release/windows/amd64 drone.exe
 tar -cvzf release/drone_darwin_amd64.tar.gz  -C release/darwin/amd64  drone
 
 # generate shas for tar files
