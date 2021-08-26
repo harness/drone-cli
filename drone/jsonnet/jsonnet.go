103241a8df94097fd9af35dd20770239e8d78615
Julien Duchesne <julien.duchesne@grafana.com>
2021-08-26 08:27:33 -0400

    Fix `--stream` combined with `--format` for `jsonnet` If both are defined and jsonnet defines a list, it currently only generates one of the resources Also added a test for it

--- drone/jsonnet/jsonnet.go
+++ drone/jsonnet/jsonnet.go
@@ -1,17 +1,11 @@
 package jsonnet
 
 import (
-	"bytes"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
-	"strings"
 
-	"github.com/fatih/color"
-	"github.com/ghodss/yaml"
-	"github.com/google/go-jsonnet"
 	"github.com/urfave/cli"
 )
 
@@ -60,81 +54,17 @@ var Command = cli.Command{
 }
 
 func generate(c *cli.Context) error {
-	source := c.String("source")
-	target := c.String("target")
-
-	data, err := ioutil.ReadFile(source)
+	result, err := convert(c.String("source"), c.Bool("string"), c.Bool("format"), c.Bool("stream"), c.StringSlice("extVar"))
 	if err != nil {
 		return err
 	}
 
-	vm := jsonnet.MakeVM()
-	vm.MaxStack = 500
-	vm.StringOutput = c.Bool("string")
-	vm.ErrorFormatter.SetMaxStackTraceSize(20)
-	vm.ErrorFormatter.SetColorFormatter(
-		color.New(color.FgRed).Fprintf,
-	)
-
-	// register native functions
-	RegisterNativeFuncs(vm)
-
-	// extVars
-	vars := c.StringSlice("extVar")
-	for _, v := range vars {
-		name, value, err := getVarVal(v)
-		if err != nil {
-			return err
-		}
-		vm.ExtVar(name, value)
-	}
-
-	buf := new(bytes.Buffer)
-	if c.Bool("stream") {
-		docs, err := vm.EvaluateSnippetStream(source, string(data))
-		if err != nil {
-			return err
-		}
-		for _, doc := range docs {
-			buf.WriteString("---")
-			buf.WriteString("\n")
-			buf.WriteString(doc)
-		}
-	} else {
-		result, err := vm.EvaluateSnippet(source, string(data))
-		if err != nil {
-			return err
-		}
-		buf.WriteString(result)
-	}
-	// enable yaml output
-	if c.Bool("format") {
-		formatted, yErr := yaml.JSONToYAML(buf.Bytes())
-		if yErr != nil {
-			return fmt.Errorf("failed to convert to YAML: %v", yErr)
-		}
-		buf.Reset()
-		buf.Write(formatted)
-	}
 	// the user can optionally write the yaml to stdout. This is useful for debugging purposes without mutating an existing file.
 	if c.Bool("stdout") {
-		io.Copy(os.Stdout, buf)
+		io.WriteString(os.Stdout, result)
 		return nil
 	}
 
-	return ioutil.WriteFile(target, buf.Bytes(), 0644)
-}
-
-// https://github.com/google/go-jsonnet/blob/master/cmd/jsonnet/cmd.go#L149
-func getVarVal(s string) (string, string, error) {
-	parts := strings.SplitN(s, "=", 2)
-	name := parts[0]
-	if len(parts) == 1 {
-		content, exists := os.LookupEnv(name)
-		if exists {
-			return name, content, nil
-		}
-		return "", "", fmt.Errorf("environment variable %v was undefined", name)
-	}
-	return name, parts[1], nil
+	target := c.String("target")
+	return ioutil.WriteFile(target, []byte(result), 0644)
 }
